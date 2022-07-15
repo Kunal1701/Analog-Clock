6402fb5a6e2397b547185b08a40f5a1dd4a72c06
Kunal Goyal <80157692+Kunal1701@users.noreply.github.com>
2022-07-15 13:22:25 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,11 +1,15 @@
 setInterval(() => {
   date = new Date();
+  console.log(date);
   hour = date.getHours();
   mi = date.getMinutes();
   sec = date.getSeconds();
   hrotation = 30 * hour + mi / 2;
   mrotation = 6 * mi;
   srotation = 6 * sec;
+  second = document.getElementById('second');
+  min = document.getElementById('min');
+  hour = document.getElementById('hour');
   second.style.transform = `rotate(${srotation}deg)`;
   min.style.transform = `rotate(${mrotation}deg)`;
   hour.style.transform = `rotate(${hrotation}deg)`;
